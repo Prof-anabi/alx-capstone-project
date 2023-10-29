170b4febf3fea461faf4964bd09b90253d5e1b6b
Anabi Asah <anabiasah@gmail.com>
2023-10-29 01:35:29 +0000

    adding testimonial section component

---
+++ project/src/Testimonial/Testimonial.js
@@ -0,0 +1,33 @@
+import React from "react";
+import classes from './Testimonial.module.css';
+import '../index.css';
+
+
+const Testimonial = (props) => {
+    const TestimonialCard = props.data.TestimonialArray.map((item) => {
+        
+        return (
+            <div key={item.id} className={classes.TestimonialSlide}>
+                <img src={item.image} alt="Person"/>
+                <h3>{item.name}</h3>
+                <p>{item.text}</p>
+            </div>
+        )
+    });
+    return (
+        <div className={classes.TestimonialContainer}>
+            <h2>Valuable <span>Testimonials</span></h2>
+            
+            <div className={classes.TestimonialWrapper}>
+                <div className={classes.TestimonialBox}>
+                    <div className={classes.TestimonialContent}>
+                    {TestimonialCard}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+  };
+
+
+export default Testimonial;
